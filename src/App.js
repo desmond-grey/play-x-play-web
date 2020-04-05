4f0ed09e7174ea3488101f04fdc6fed46811f6f0
desmond <desmond@greywether.com>
2020-04-04 23:42:34 -0700

    Revamp layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col>Foo</Col>
-          <Col>Bar</Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App row vh-100">
+            <div className="col align-self-center">
+                foo
+            </div>
+            <div className="col align-self-center">
+                bar
+            </div>
+        </div>
+    );
 }
 
 export default App;
