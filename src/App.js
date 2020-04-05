08cd0b2fb1eb1ba53ac85f5c1b891d065932bd36
desmond <desmond@greywether.com>
2020-04-04 18:47:56 -0700

    Add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Row>
+          <Col>Foo</Col>
+          <Col>Bar</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
