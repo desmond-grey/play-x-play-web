efe0d83a4d700f99aacde00307bbb4ec3682508b
desmond <desmond@greywether.com>
2020-04-06 23:09:31 -0700

    Component-ize display of Player info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.POLLING_INTERVAL_MILLIS = 250;
+        this.POLLING_INTERVAL_MILLIS = 1000;
         this.PXP_GAME_STATUS_ENDPOINT = 'http://172.16.0.16:3000/ping-pong/tables/prototype_controller/game';
 //        this.PXP_GAME_STATUS_ENDPOINT = 'http://ec2-35-166-35-105.us-west-2.compute.amazonaws.com:3000/ping-pong/tables/prototype_controller/game';
         this.pollingInterval = null;
@@ -31,10 +31,20 @@ class App extends Component {
         return (
             <div className="App row vh-100">
                 <div className="col align-self-center">
-                    {this.state.gameState ? this.state.gameState.players[0].id : '---'}
+                    {this.state.gameState &&
+                        <Player
+                            pointsScored={this.state.gameState.players[0].pointsScored}
+                            playerId={this.state.gameState.players[0].id}
+                        />
+                    }
                 </div>
                 <div className="col align-self-center">
-                    {this.state.gameState ? this.state.gameState.players[1].id : '---'}
+                    {this.state.gameState &&
+                        <Player
+                            pointsScored={this.state.gameState.players[1].pointsScored}
+                            playerId={this.state.gameState.players[1].id}
+                        />
+                    }
                 </div>
             </div>
         );
@@ -42,7 +52,6 @@ class App extends Component {
 
     async getGameState() {
         console.log('Getting game state');
-
         fetch(this.PXP_GAME_STATUS_ENDPOINT)
             .then(throwErrorIfResponseIsNotOk)
             .then(response => response.text())      // todo: this line, and the line below, should not be necessary.  should just be able to call response.json() function
@@ -58,5 +67,13 @@ function throwErrorIfResponseIsNotOk(response) {
     return response;
 }
 
+function Player(props) {
+    return (
+        <div>
+            <h1>{props.pointsScored}</h1>
+            <h2>{props.playerId}</h2>
+        </div>
+    );
+}
 
 export default App;
